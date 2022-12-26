4b5c04eef92d11b96e666183fbad6398150c5ac6
VASPARITHARUNKUMAR <103122665+VASPARITHARUNKUMAR@users.noreply.github.com>
2022-12-26 05:47:08 +0530

    Add files via upload

---
+++ maxheight.py
@@ -0,0 +1,4 @@
+n=int(input("enter a number"))
+g=float(input(9.8))
+h=(n^2)/2*g
+print("max height:",h)
---
+++ mul2nosusing+.py
@@ -0,0 +1,6 @@
+n=int(input("enter 1st number"))
+a=int(input("enter 2nd number"))
+product=0
+for i in range(1,a+1):
+   product=product+n
+print("multiplication of 2 nos:",product)
